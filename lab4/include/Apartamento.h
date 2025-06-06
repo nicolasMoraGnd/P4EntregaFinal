b4f0f7a44d3c173a55ab1d02612f704f3e65d5d2
marianiwis <marichulaprof@gmail.com>
2025-06-06 14:44:45 -0300

    aver

--- lab4/include/Apartamento.h
+++ lab4/include/Apartamento.h
@@ -1,18 +1,10 @@
 #ifndef APARTAMENTO_H
 #define APARTAMENTO_H
-#include "Inmueble.h"
 
 
-class Apartamento : public Inmueble {
+class Apartamento : public Inmueble{
     private:
-        int piso;
-        bool tieneAscensor;
-        float gastosComunes;
-
     public:
-        Apartamento(int piso, bool tieneAscensor, float gastosComunes);
-        bool esCasa();
-        ~Apartamento();
 };
 
 #endif
