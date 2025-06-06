55a605eafe3bbe8a2a40228dbcf6c2e9c7f1051f
marianiwis <marichulaprof@gmail.com>
2025-06-06 14:47:34 -0300

    wtf prt2

--- lab4/include/Casa.h
+++ lab4/include/Casa.h
@@ -1,17 +1,9 @@
 #ifndef CASA_H
 #define CASA_H
 #include "Inmueble.h"
-#include "TipoTecho.h"
 
-class Casa : public Inmueble {
-    private:
-        bool esPH;
-        TipoTecho techo;
 
     public:
-        Casa(bool esPH, TipoTecho techo);
-        bool esCasa();
-        ~Casa();
 };
 
 #endif
--- lab4/include/Inmueble.h
+++ lab4/include/Inmueble.h
@@ -2,7 +2,6 @@
 #define INMUEBLE_H
 #include <string>
 
-class Inmueble {
     private:
         int codigo;
         std::string direccion;
@@ -11,9 +10,5 @@ class Inmueble {
         int anoConstruccion;
 
     public:
-        Inmueble(int codigo, std::string direccion, int numeroPuerta, int superficie, int anoConstruccion);
-        bool esCasa();
-        ~Inmueble();
 };
 
-#endif
