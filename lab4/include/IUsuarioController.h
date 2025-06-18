a24c444a1581f348b59ca46b5176d02dc016455c
Nicolas Mora <89613192+nicolasMoraGnd@users.noreply.github.com>
2025-06-18 20:16:43 -0300

    IUsuarioController

---
+++ lab4/include/IUsuarioController.h
@@ -0,0 +1,25 @@
+#ifndef IUSUARIOCONTROLLER_H
+#define IUSUARIOCONTROLLER_H
+
+#include <set>
+#include <string>
+
+struct DTInmuebleAdministrado;
+struct DTUsuario;
+
+class IUsuarioController{
+    public:
+    private:
+        virtual ~IUsuarioController(){}
+
+        virtual bool altaCliente(const std::string& nickname, const std::string& constrasena, const std::string& email, const std::string& apellido, const std::string& documento) = 0;
+        virtual bool altaPropietario(const std::string& nickname, const std::string& constrasena, const std::string& email, const std::string& cuentaBancaria, const std::string& telefono) = 0;
+        virtual bool altaInmobiliaria(const std::string& nickname, const std::string& constrasena, const std::string& email, const std::string& direccion, const std::string& url, const std::string& telefono) = 0;
+
+        virtual std::set<DTUsuario*> listarInmobiliarias() = 0;
+
+        virtual std::set<DTInmuebleAdministrado*> listarInmueblesAdministrados(const std::string& nicknameInmobiliaria) = 0;
+
+}:
+
+#endif
